2dfd72b55931799b4644c75e8f1e2644d21b79d7
Joni Turunen <joni.turunen@outlook.com>
2021-01-10 19:37:52 +0200

    task: Add annotate, annotate bg Add cli argument support Add s3 fileuploader

--- snapshot.py
+++ snapshot.py
@@ -1,11 +1,11 @@
 from time import sleep
-from picamera import PiCamera
-import signal, sys
+import signal, sys, time, datetime, picamera
 from picamutils.time import Timegiver
 from picamutils.aws import S3FileUploader
 
 ts = Timegiver()
 filename = 'tmp/snapshot.jpg'
+starttime = time.time()
 
 def signal_handler(sig, frame):
 	print("\n{ts.generateTimestamp()} ::QUIT:: `CTRL + C` received! Closing application.")
@@ -14,15 +14,34 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 print(f'{ts.generateTimestamp()} ::START:: Starting image capturing...\n{ts.generateTimestamp()} ::INFO:: Press `CTRL + C` to exit.')
 
-def take_a_shot(rotation=90):
-	camera = PiCamera()
+def take_a_shot(rotation=180):
+	camera = picamera.PiCamera()
 	camera.rotation = rotation
 	try:
 		camera.start_preview()
+		camera.annotate_background = picamera.Color('Black')
+		camera.annotate_text_size = 12
+		camera.annotate_text = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 		sleep(2)
 		camera.capture(filename)
+		s3 = S3FileUploader(filename, 'latest.jpg')
+		fileupload = s3.upload_file_to_bucket()
+
 	finally:
 		camera.close()
-		print(f'{ts.generateTimestamp()} ::DONE:: Image {filename} saved. \n{ts.generateTimestamp()} ::QUIT:: Exiting application..')
+		print(f'{ts.generateTimestamp()} ::INFO:: Image {filename} saved.')
 
-take_a_shot()
+
+
+if __name__ == '__main__':
+	interval = 1800.0 # 30min
+
+	if len(sys.argv) > 1:
+		print(f'{ts.generateTimestamp()} ::INFO:: Script started with additional arguments:"{str(sys.argv[1:])}')
+		if float(sys.argv[1]) > 1800:
+			interval = float(sys.argv[1])
+	print(f'{ts.generateTimestamp()} ::INFO:: Image capture interval set to {interval}s.')
+
+	while True:
+		take_a_shot(180)
+		time.sleep(interval - ((time.time() - starttime) % interval))
